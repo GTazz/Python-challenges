5041522b4f7d8e78c4688692aa65a1c5009a221b
Gabriel Tazz <gabriel.oliveira.tazz@gmail.com>
2025-04-25 17:07:39 -0300

    Refactor: Simplify the pyramid.py in a single line of code and let it generate in reverse

--- challenges/1-pyramid.py
+++ challenges/1-pyramid.py
@@ -1,2 +1,5 @@
-height = int(input("\nPyramid height: ")) * 2
-print("".join([f"\n{'*'+'*'*i:^{height}}" for i in range(0, height, 2)]) + "\n")
+print((lambda h: "".join([f"\n{'*'+'*'*i:^{abs(h)}}" for i in range(*(0, h, 2) if h > -1 else (abs(h+2), -1, -2))]) + "\n")(int(input("\nPyramid height: ")) * 2))
+
+# Everything in a fucking one line of fucking python fucking code. And it works for fucking reversed pyramids as well.
+
+# Code Repo: https://github.com/GTazz/Python-challenges/blob/main/challenges/1-pyramid.py
